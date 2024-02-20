b5caf52279cbe956b7e5ecaf212302013b6aae51
jinguman <microants@jinguman.cn>
2024-02-20 20:37:16 +0800

    init

---
+++ biz/src/test/java/com/jingu/biz/BizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jingu.biz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
