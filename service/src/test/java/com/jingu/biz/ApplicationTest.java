fce823a5154aafe52ed764c7b383b253be2e3939
jingu <228385607@qq.com>
2025-04-07 13:33:35 +0800

    test module

---
+++ service/src/test/java/com/jingu/biz/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.jingu.biz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+public class ApplicationTest {
+
+    @Test
+    public void test() {
+        System.out.println("Hello World");
+    }
+
+}
